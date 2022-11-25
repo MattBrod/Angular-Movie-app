fee2ec8b99ff625080a4f5e51e5ea522fb04b764
MattBrod <66639698+MattBrod@users.noreply.github.com>
2022-11-26 00:15:57 +0100

    Estructura

--- peliculasPlus/src/app/app.module.ts
+++ peliculasPlus/src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { DetailComponent } from './detail/detail.component';
+import { AboutComponent } from './about/about.component';
+import { ProfileComponent } from './profile/profile.component';
+import { CrudComponent } from './crud/crud.component';
+import { GenreComponent } from './genre/genre.component';
+import { ElementsModule } from './elements/elements.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    DetailComponent,
+    AboutComponent,
+    ProfileComponent,
+    CrudComponent,
+    GenreComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ElementsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
